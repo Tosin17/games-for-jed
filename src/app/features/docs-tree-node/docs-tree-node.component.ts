3fa388581285d6632ea039ac39da87b23db86b84
Tosin17 <kajopelayeola@gmail.com>
2019-08-18 22:50:13 -0400

    Add recursive ngFor

--- src/app/features/docs-tree-node/docs-tree-node.component.ts
+++ src/app/features/docs-tree-node/docs-tree-node.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { INode } from '../../models/docs';
+import { get } from 'lodash';
 
 @Component({
   selector: 'app-docs-tree-node',
   templateUrl: './docs-tree-node.component.html',
   styleUrls: ['./docs-tree-node.component.scss']
 })
-export class DocsTreeNodeComponent implements OnInit {
-  @Input() model;
+export class DocsTreeNodeComponent implements OnInit, OnChanges {
+  @Input() nodeData: INode;
+  private hasChildren: boolean;
+  private showChildren: boolean;
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.model);
+    // if (this.nodeData.ChildNodes.length) {
+    // }
   }
 
+  ngOnChanges() {
+    this.hasChildren = !!get(this.nodeData, 'ChildNodes.length');
+  }
+
+  trackByFn(index, node) {
+    return get(node, 'node.Name') || null;
+  }
+
+  nodeSelected(evt, node) {
+    evt.stopPropagation();
+    // console.log(node);
+    if (node.ChildNodes.length) {
+      this.showChildren = !this.showChildren;
+    }
+  }
 }
