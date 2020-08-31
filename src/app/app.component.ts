16f76caeae266c7e3d231fe41fbae62ec253cde3
Tosin17 <kajopelayeola@gmail.com>
2020-08-31 10:49:27 -0400

    Add router vents for gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { TrackUserEventsService } from '../app/shared/services/track-user-events.service';
+import { Component, OnInit } from "@angular/core";
+import { TrackUserEventsService } from "../app/shared/services/track-user-events.service";
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  constructor(private trackUserService: TrackUserEventsService) { }
+  constructor(
+    private router: Router,
+    private trackUserService: TrackUserEventsService
+  ) {
+    const global: any = window;
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        global.ga("set", "page", event.urlAfterRedirects);
+        global.ga("send", "pageview");
+      }
+    });
+  }
 
   ngOnInit() {
     this.trackUserService.initializeEventsTracker();
