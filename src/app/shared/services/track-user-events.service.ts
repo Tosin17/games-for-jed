519cbf8fe57f134fc69fe94d23f8d8a63ab3d098
Tosin <kajopelayeola@gmail.com>
2021-02-03 03:25:19 -0500

    Update track-user-events.service.ts

--- src/app/shared/services/track-user-events.service.ts
+++ src/app/shared/services/track-user-events.service.ts
@@ -1,48 +1,44 @@
 import { Injectable } from '@angular/core';
 import { fromEvent, merge, interval } from 'rxjs';
-import { switchMap, scan, takeWhile, tap, startWith, map } from 'rxjs/operators';
+import {
+  switchMap,
+  scan,
+  takeWhile,
+  startWith,
+  mapTo,
+  tap,
+} from 'rxjs/operators';
+
+const l = console.log;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TrackUserEventsService {
-  // Number of seconds user has to stay idle before he's logged out
-  private readonly COUNT_IN_SECONDS: number = 3600;
+  private readonly COUNT_IN_SECONDS: number = 30000;
 
-  constructor() { }
+  constructor() {}
 
-  initializeEventsTracker() {
-    // At 1 second interval, return interval and -1 for counting down
-    const interval$ = interval(1000).pipe(map(tick => ({ tick, val: -1 })));
+  initializeEventsTracker(): void {
+    const interval$ = interval(1000).pipe(mapTo(1));
 
-    // Events to track
-    const onload$ = fromEvent(document, 'onload');
     const keyboard$ = fromEvent(document, 'keyup');
     const click$ = fromEvent(document, 'click');
     const mousemove$ = fromEvent(document, 'mousemove');
-    const scroll$ = fromEvent(document, 'scroll');
 
-    // If any of this event occurs, emit the event
-    merge(onload$, keyboard$, click$, mousemove$, scroll$).pipe(
-      // Start the count down
-      startWith(interval$),
-      // Reset the timer anytime the user types, scrolls or uses mouse
-      switchMap(() => interval$),
-      // Calculate the count down by adding -1 + COUNT_IN_SECONDS everytime an evt occurs
-      scan((acc, curr: any) => {
-        // If `switchMap` restarts the counter, reset the accumulator
-        if (curr.tick === 0) {
-          acc = this.COUNT_IN_SECONDS;
-          return acc;
-        }
-        return acc + curr.val;
-      }, this.COUNT_IN_SECONDS),
-      takeWhile(tick => tick >= 0)
-    ).subscribe(tick => {
-      // When counter completes, log user out or do whatever
-      if (tick === 0) {
-        console.log('You wonna log out now?');
-      }
-    });
+    merge(keyboard$, click$, mousemove$)
+      .pipe(
+        startWith(interval$),
+        switchMap(() => interval$.pipe(scan((acc, curr) => acc + curr))),
+        takeWhile((tick) => tick <= this.COUNT_IN_SECONDS),
+        tap((tick) => {
+          if (tick === this.COUNT_IN_SECONDS - 10) {
+            l('Are you still there?'); // Open modal
+          } else if (tick === this.COUNT_IN_SECONDS) {
+            l('You have been logged out') // Log out
+          }
+        })
+      )
+      .subscribe();
   }
 }
