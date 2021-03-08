4cf61574f5bed66e1a627a4cc97d0ae4879d3fcd
Tosin17 <kajopelayeola@gmail.com>
2021-03-08 00:50:24 -0500

    Clean ups

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { DocsViewComponent } from "./features/docs-view/docs-view.component";
 import { AlphabetInvasionComponent } from "./core/alphabet-invasion/alphabet-invasion.component";
-import { PracticeComponent } from "./core/practice/practice.component";
 import { CountdownTimerComponent } from "./features/countdown-timer/countdown-timer.component";
 
 const routes: Routes = [
