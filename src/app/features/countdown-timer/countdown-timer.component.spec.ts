2173c58677c7f9b19a58ac7dc976ed9260dd1472
Tosin17 <kajopelayeola@gmail.com>
2021-03-09 00:49:04 -0500

    Add jest

--- src/app/features/countdown-timer/countdown-timer.component.spec.ts
+++ src/app/features/countdown-timer/countdown-timer.component.spec.ts
@@ -1,25 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { noop } from "rxjs";
+import { CountdownTimerComponent } from "./countdown-timer.component";
 
-import { CountdownTimerComponent } from './countdown-timer.component';
+describe("CountdownTimerComponent", () => {
+  const component = new CountdownTimerComponent({ emit: noop });
 
-describe('CountdownTimerComponent', () => {
-  let component: CountdownTimerComponent;
-  let fixture: ComponentFixture<CountdownTimerComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CountdownTimerComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CountdownTimerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should init component", () => {
+    expect(component).toBeDefined();
   });
 });
