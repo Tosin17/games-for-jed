341509081d13ea087d9bf6caa4c5cd3ebd1969e2
Tosin17 <kajopelayeola@gmail.com>
2021-04-18 04:02:47 -0400

    Build alphabet invasion game

--- src/app/core/alphabet-invasion/alphabet-invasion.component.ts
+++ src/app/core/alphabet-invasion/alphabet-invasion.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  BehaviorSubject,
+  combineLatest,
+  fromEvent,
+  interval,
+  noop,
+} from "rxjs";
+import {
+  switchMap,
+  scan,
+  startWith,
+  map,
+  tap,
+  take,
+  takeWhile,
+  finalize,
+} from "rxjs/operators";
+import { last } from "lodash";
+
+interface ILetters {
+  intrvl: number;
+  ltrs: any[];
+}
+
+interface IState {
+  score: number;
+  letters: any[];
+}
 
 @Component({
-  selector: 'alphabet-invasion',
-  templateUrl: './alphabet-invasion.component.html',
-  styleUrls: ['./alphabet-invasion.component.scss']
+  selector: "alphabet-invasion",
+  templateUrl: "./alphabet-invasion.component.html",
+  styleUrls: ["./alphabet-invasion.component.scss"],
 })
-export class AlphabetInvasionComponent implements OnInit {
+export class AlphabetInvasionComponent {
+  constructor() {}
 
-  constructor() { }
+  randomLetter = () =>
+    String.fromCharCode(
+      Math.random() * ("z".charCodeAt(0) - "a".charCodeAt(0)) +
+        "a".charCodeAt(0)
+    );
 
-  ngOnInit() {
-  }
+  gameWidth = 200;
+  threshold = 15;
 
+  keys$ = fromEvent(document, "keydown").pipe(
+    startWith({ key: "" }),
+    map((e: KeyboardEvent) => e.key)
+  );
+
+  intervalSub = new BehaviorSubject(1000);
+  gameOverSub = new BehaviorSubject(false);
+
+  letters$ = this.intervalSub.pipe(
+    switchMap((i) =>
+      interval(i).pipe(
+        scan<number, ILetters>(
+          (letters) => ({
+            intrvl: i,
+            ltrs: [
+              {
+                letter: this.randomLetter(),
+                xPos: Math.floor(Math.random() * this.gameWidth),
+              },
+              ...letters.ltrs,
+            ],
+          }),
+          { intrvl: 0, ltrs: [] }
+        )
+      )
+    )
+  );
+
+  game$ = combineLatest(this.keys$, this.letters$).pipe(
+    scan<[string, ILetters], IState>(
+      (state, [key, letters]) => {
+        const latest = last(letters.ltrs);
+        latest.letter === key
+          ? (letters.ltrs.pop(), (state.score += 1))
+          : noop();
+        return {
+          score: state.score,
+          letters: letters.ltrs,
+        };
+      },
+      {
+        score: 0,
+        letters: [],
+      }
+    ),
+    takeWhile((state) => state.letters.length < 15),
+    finalize(() => this.gameOverSub.next(true))
+  );
 }
---
+++ src/app/core/alphabet-invasion/snippet.ts
@@ -0,0 +1,34 @@
+import { BehaviorSubject, interval, noop } from "rxjs";
+import { switchMap, scan, take } from "rxjs/operators";
+
+const log = console.log;
+
+const randomLetter = () => "a";
+const gameWidth = 30;
+
+const intervalSub = new BehaviorSubject(600);
+
+const letters$ = intervalSub.pipe(
+  switchMap((i) =>
+    interval(i).pipe(
+      scan<any>(
+        (letters, v) => ({
+          v,
+          intrvl: i,
+          ltrs: [
+            {
+              letter: randomLetter(),
+              yPos: Math.floor(Math.random() * gameWidth),
+            },
+            ...letters.ltrs,
+          ],
+        }),
+        { intrvl: 0, ltrs: [] }
+      ),
+      take(5)
+    )
+  )
+);
+
+intervalSub.next(600);
+letters$.subscribe(console.log);
